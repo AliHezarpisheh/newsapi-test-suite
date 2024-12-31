96279c51bee5d9d51b6545f147da8eaa52cdedd5
ali <alihezarpisheh@outlook.com>
2024-12-31 12:31:17 +0330

    🔐 Feature: Add project `Settings` and env example

---
+++ config/settings.py
@@ -0,0 +1,31 @@
+"""Module for handling settings in the application."""
+
+from functools import lru_cache
+from typing import Annotated
+
+from pydantic import Field
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    """Class for handling all the settings in the application."""
+
+    # API Settings
+    BASE_URL: Annotated[
+        str, Field(description="The base url for the API getting tested")
+    ]
+    API_KEY: Annotated[str, Field(description="The API key, used for auth")]
+
+    # Settings Configuration
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        case_sensitive=False,
+        use_enum_values=True,
+        extra="ignore",
+    )
+
+
+@lru_cache
+def get_settings() -> Settings:
+    """Return the instance of `Settings`, using lru_caching."""
+    return Settings()
