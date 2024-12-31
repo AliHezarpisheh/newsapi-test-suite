f920af1df5c5534485eaf7cb980d006838e7f909
ali <alihezarpisheh@outlook.com>
2024-12-31 13:01:35 +0330

    ✨ Feature(toolkit): Add utilities for the project
    - Implement sync and async API clients. Sync API clients
      is going to be used for pytest-bdd tests, due to lack of
      async support in pytest-bdd.
    - Add enumerations based on the NewsAPI documentation.

---
+++ toolkit/__init__.py
@@ -0,0 +1,4 @@
+from .api_clients import APIClient, AsyncAPIClient
+from .enums import ResponseCodeEnum, ResponseStatusEnum
+
+__all__ = ["APIClient", "AsyncAPIClient", "ResponseCodeEnum", "ResponseStatusEnum"]
---
+++ toolkit/enums.py
@@ -0,0 +1,20 @@
+"""Module providing enumeration classes for response statuses and codes."""
+
+from enum import Enum
+
+
+class ResponseStatusEnum(str, Enum):
+    """Enumeration of possible response statuses."""
+
+    OK = "ok"
+    ERROR = "error"
+
+
+class ResponseCodeEnum(str, Enum):
+    """Enumeration of possible response codes."""
+
+    API_KEY_MISSING = "apiKeyMissing"
+    API_KEY_INVALID = "apiKeyInvalid"
+    PARAMETER_INVALID = "parameterInvalid"
+    PARAMETER_MISSING = "parametersMissing"
+    SOURCES_DOES_NOT_EXIST = "sourceDoesNotExist"
